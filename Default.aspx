d45266d659b7755d0c1a8fa6d56852dc277e0ce3
AJ's World <AJ's World@AJ>
2017-02-19 20:50:23 +0530

    Add project files.

---
+++ Default.aspx
@@ -0,0 +1,104 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?sensor=false&libraries=places"></script>
+    <script type="text/javascript">
+        var source, destination;
+        var directionsDisplay;
+
+        var retriction = { componentRestrictions: { 'country': 'in' } };
+
+        var directionsService = new google.maps.DirectionsService();
+        google.maps.event.addDomListener(window, 'load', function () {
+            new google.maps.places.SearchBox(document.getElementById('txtSource'));
+            new google.maps.places.SearchBox(document.getElementById('txtDestination'));
+            directionsDisplay = new google.maps.DirectionsRenderer({ 'draggable': true });
+        });
+
+        function GetRoute() {
+            var mumbai = new google.maps.LatLng(18.9750, 72.8258);
+            var mapOptions = {
+                zoom: 7,
+                center: mumbai
+            };
+            map = new google.maps.Map(document.getElementById('dvMap'), mapOptions);
+            directionsDisplay.setMap(map);
+            directionsDisplay.setPanel(document.getElementById('dvPanel'));
+
+            //*********DIRECTIONS AND ROUTE**********************//
+            source = document.getElementById("txtSource").value;
+            destination = document.getElementById("txtDestination").value;
+
+            //source = "";
+            alert(source);
+            alert(destination);
+            var request = {
+                origin: source,
+                destination: destination,
+                travelMode: google.maps.TravelMode.DRIVING
+            };
+            directionsService.route(request, function (response, status) {
+                if (status == google.maps.DirectionsStatus.OK) {
+                    directionsDisplay.setDirections(response);
+                }
+            });
+
+            //*********DISTANCE AND DURATION**********************//
+            //var service = new google.maps.DistanceMatrixService();
+            //service.getDistanceMatrix({
+            //    origins: [source],
+            //    destinations: [destination],
+            //    travelMode: google.maps.TravelMode.DRIVING,
+            //    unitSystem: google.maps.UnitSystem.METRIC,
+            //    avoidHighways: false,
+            //    avoidTolls: false
+            //}, function (response, status) {
+            //    if (status == google.maps.DistanceMatrixStatus.OK && response.rows[0].elements[0].status != "ZERO_RESULTS") {
+            //        var distance = response.rows[0].elements[0].distance.text;
+            //        var duration = response.rows[0].elements[0].duration.text;
+            //        var dvDistance = document.getElementById("dvDistance");
+            //        dvDistance.innerHTML = "";
+            //        dvDistance.innerHTML += "Distance: " + distance + "<br />";
+            //        dvDistance.innerHTML += "Duration:" + duration;
+
+            //    } else {
+            //        alert("Unable to find the distance via road.");
+            //    }
+            //});
+        }
+    </script>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:TextBox ID="txtSource" runat="server"></asp:TextBox>
+            <asp:TextBox ID="txtDestination" runat="server"></asp:TextBox>
+
+            <asp:Button ID="btnCal" runat="server" Text="Calculate" OnClick="btnCal_Click" />
+
+            <input type="button" value="Get Route" onclick="GetRoute()" />
+
+            <br />
+            <br />
+            <b>Time: </b>
+            <asp:Label ID="lblDuration" runat="server"></asp:Label>
+            <br />
+            <b>Distance: </b>
+            <asp:Label ID="lblDist" runat="server"></asp:Label>
+        </div>
+        <div id="dvDistance">
+        </div>
+        <div>
+            <asp:GridView ID="grdMapping" runat="server"></asp:GridView>
+        </div>
+        <div id="dvMap" style="width: 500px; height: 500px">
+        </div>
+        <%--<div id="dvPanel" style="width: 500px; height: 500px">
+        </div>--%>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Net;
+using System.IO;
+using System.Xml;
+using System.Data;
+using System.Data.SqlClient;
+using System.Threading;
+
+public partial class _Default : System.Web.UI.Page
+{
+    string connectionString = "Data Source=Ajendra;Initial Catalog=EzeeMoving;Integrated Security=True";
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        //calcDistance();
+    }
+
+    protected void btnCal_Click(object sender, EventArgs e)
+    {
+        getdistance(txtSource.Text.ToString(),txtDestination.Text.ToString());
+    }
+
+    public void getdistance(string origin, string destination)
+    {
+        string url = @"http://maps.googleapis.com/maps/api/distancematrix/xml?origins=" + origin + "&destinations=" + destination + "&sensor=false";
+
+        HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+        WebResponse response = request.GetResponse();
+        Stream datastream = response.GetResponseStream();
+        StreamReader sreader = new StreamReader(datastream);
+        string responseReader = sreader.ReadToEnd();
+        response.Close();
+
+        DataSet ds = new DataSet();
+        ds.ReadXml( new XmlTextReader(new StringReader(responseReader)));
+        if (ds.Tables.Count > 0)
+        {
+            if (ds.Tables["element"].Rows[0]["status"].ToString() == "OK")
+            {
+                lblDuration.Text = ds.Tables["duration"].Rows[0]["text"].ToString();
+                lblDist.Text = ds.Tables["distance"].Rows[0]["text"].ToString();
+            }
+        }
+    }
+
+    public void calcDistance()
+    {
+        List<string> lst1 = new List<string>();
+        List<string> lst2 = new List<string>();
+        DataTable dt = new DataTable();
+        string url = "";
+        HttpWebRequest request;
+        WebResponse response;
+        Stream datastream;
+        StreamReader sreader;
+        string responseReader;
+        DataSet ds;
+        List<LocationMapping> lst3 = new List<LocationMapping>();
+
+        SqlConnection cn = new SqlConnection(connectionString);
+        cn.Open();
+        SqlCommand cmd = new SqlCommand("select LocationDesc from locationmaster", cn);
+        SqlDataAdapter da = new SqlDataAdapter(cmd);
+        da.Fill(dt);
+        if (dt.Rows.Count > 0)
+        {
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                lst1.Add(dt.Rows[i]["LocationDesc"].ToString() + ", Mumbai");
+                lst2.Add(dt.Rows[i]["LocationDesc"].ToString() + ", Mumbai");
+            }
+
+            for (int j = 0; j < lst1.Count ; j++)
+            {
+                for (int k = 0; k < lst2.Count ; k++)
+                {
+                    url = @"http://maps.googleapis.com/maps/api/distancematrix/xml?origins=" + lst1[j].ToString() + "&destinations=" + lst2[k].ToString() + "&sensor=false";
+                    request = (HttpWebRequest)WebRequest.Create(url);
+                    response = request.GetResponse();
+                    datastream = response.GetResponseStream();
+                    sreader = new StreamReader(datastream);
+                    responseReader = sreader.ReadToEnd();
+                    response.Close();
+                    ds = new DataSet();
+                    ds.ReadXml(new XmlTextReader(new StringReader(responseReader)));
+                    if (ds.Tables.Count > 0)
+                    {
+                        if (ds.Tables["element"].Rows[0]["status"].ToString() == "OK")
+                        {
+                            LocationMapping map = new LocationMapping();
+
+                            map.Source = lst1[j].ToString();
+                            map.Destination = lst2[k].ToString();
+                            map.Distance = ( ds.Tables["distance"].Rows[0]["text"].ToString());
+                            lst3.Add(map);
+                            //lblDist.Text = ds.Tables["distance"].Rows[0]["text"].ToString();
+                        }
+                    }
+                    Thread.Sleep(200);
+                }
+            }
+            grdMapping.DataSource = lst3;
+            grdMapping.DataBind();
+        }
+
+    }
+}
+
+partial class LocationMapping
+{
+    string _source = string.Empty;
+    string _destination = string.Empty;
+    string _distance = string.Empty;
+
+    public string Source
+    {
+        get { return _source; }
+        set { _source = value; }
+    }
+    public string Destination
+    {
+        get { return _destination; }
+        set { _destination = value; }
+    }
+    public string Distance
+    {
+        get { return _distance; }
+        set { _distance = value; }
+    }
+}
